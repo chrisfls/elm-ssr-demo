4f78aab9567a8cfeb509b82f3eacab124db4f06a
Chris <2013206+kress95@users.noreply.github.com>
2023-05-20 16:38:09 -0300

    before injecting phoenix

--- shell.nix
+++ shell.nix
@@ -3,9 +3,11 @@ with pkgs;
 mkShell {
   buildInputs = with pkgs; [
     deno
+    elixir_1_14
     elmPackages.elm
     elmPackages.elm-format
     elmPackages.elm-language-server
     nodejs-16_x # needed by elm-language-server
+    postgresql_15
   ];
 }
