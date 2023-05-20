a1dda6ccf7dcffeb0c2f695d384e72ec60a48890
Chris <2013206+kress95@users.noreply.github.com>
2023-05-20 02:24:01 -0300

    split logic

--- app/browser/Main.elm
+++ app/browser/Main.elm
@@ -1,88 +1,20 @@
--- Input a user name and password. Make sure the password matches.
---
--- Read how it works:
---   https://guide.elm-lang.org/architecture/forms.html
---
-
-
-module Main exposing (Model, Msg(..), init, main, update, view, viewInput, viewValidation)
+module Main exposing (main)
 
 import Browser
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Entrypoint
 
 
-
--- MAIN
-
-
+main : Program () Entrypoint.Model Entrypoint.Msg
 main =
-    Browser.sandbox { init = init, update = update, view = view }
-
-
-
--- MODEL
-
-
-type alias Model =
-    { name : String
-    , password : String
-    , passwordAgain : String
-    }
-
-
-init : Model
-init =
-    Model "" "" ""
-
-
-
--- UPDATE
-
-
-type Msg
-    = Name String
-    | Password String
-    | PasswordAgain String
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        Name name ->
-            { model | name = name }
-
-        Password password ->
-            { model | password = password }
-
-        PasswordAgain password ->
-            { model | passwordAgain = password }
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-    div []
-        [ viewInput "text" "Name" model.name Name
-        , viewInput "password" "Password" model.password Password
-        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
-        , viewValidation model
-        ]
-
-
-viewInput : String -> String -> String -> (String -> msg) -> Html msg
-viewInput t p v toMsg =
-    input [ type_ t, placeholder p, value v, onInput toMsg ] []
-
-
-viewValidation : Model -> Html msg
-viewValidation model =
-    if model.password == model.passwordAgain then
-        div [ style "color" "green" ] [ text "OK" ]
-
-    else
-        div [ style "color" "red" ] [ text "Passwords do not match!" ]
+    Browser.application
+        { init = \flags _ _ -> Entrypoint.init flags
+        , update = Entrypoint.update
+        , view =
+            \model ->
+                { title = "Title"
+                , body = [ Entrypoint.view model ]
+                }
+        , subscriptions = \_ -> Sub.none
+        , onUrlChange = \_ -> Entrypoint.Noop
+        , onUrlRequest = \_ -> Entrypoint.Noop
+        }
---
+++ app/src/Entrypoint.elm
@@ -0,0 +1,76 @@
+module Entrypoint exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onInput)
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { name : String
+    , password : String
+    , passwordAgain : String
+    }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( Model "" "" "", Cmd.none )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Name String
+    | Password String
+    | PasswordAgain String
+    | Noop
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Name name ->
+            ( { model | name = name }, Cmd.none )
+
+        Password password ->
+            ( { model | password = password }, Cmd.none )
+
+        PasswordAgain password ->
+            ( { model | passwordAgain = password }, Cmd.none )
+
+        Noop ->
+            ( model, Cmd.none )
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ viewInput "text" "Name" model.name Name
+        , viewInput "password" "Password" model.password Password
+        , viewInput "password" "Re-enter Password" model.passwordAgain PasswordAgain
+        , viewValidation model
+        ]
+
+
+viewInput : String -> String -> String -> (String -> msg) -> Html msg
+viewInput t p v toMsg =
+    input [ type_ t, placeholder p, value v, onInput toMsg ] []
+
+
+viewValidation : Model -> Html msg
+viewValidation model =
+    if model.password == model.passwordAgain then
+        div [ style "color" "green" ] [ text "OK" ]
+
+    else
+        div [ style "color" "red" ] [ text "Passwords do not match!" ]
