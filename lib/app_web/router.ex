1282988a80a9a3cb13ce9635200f4d4e652547e7
Chris <2013206+kress95@users.noreply.github.com>
2023-05-20 23:26:02 -0300

    add absinthe

--- lib/app_web/router.ex
+++ lib/app_web/router.ex
@@ -8,4 +8,11 @@ defmodule AppWeb.Router do
   scope "/api", AppWeb do
     pipe_through :api
   end
+
+  if Mix.env == :dev do
+    forward "/graphiql",
+      Absinthe.Plug.GraphiQL,
+      schema: AppWeb.Schema,
+      interface: :playground
+  end
 end
