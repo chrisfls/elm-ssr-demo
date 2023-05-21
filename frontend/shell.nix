408fda449be6a7abb3dc2dd07b26fa6c1dce0a44
Chris <2013206+kress95@users.noreply.github.com>
2023-05-21 02:28:28 -0300

    add packages back

--- frontend/shell.nix
+++ frontend/shell.nix
@@ -3,6 +3,10 @@ with pkgs;
 mkShell {
   buildInputs = with pkgs; [
     deno
+    elmPackages.elm
+    elmPackages.elm-format
+    elmPackages.elm-live
+    elmPackages.elm-test
     nodejs-16_x # needed by elm-language-server
   ];
 }
